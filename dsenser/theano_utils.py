c1a3cfac342a4ac33d8157ccb5fdef92011c4cf6
Wladimir Sidorenko <wlsidorenko@gmail.com>
2016-05-13 19:41:03 +0200

    Update theano_utils.py

--- dsenser/theano_utils.py
+++ dsenser/theano_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8; mode: python; -*-
 
-"""Module providing common heano utils.
+"""Module providing common theano utils.
 
 Attributes:
 floatX (method): force float type expected by theano

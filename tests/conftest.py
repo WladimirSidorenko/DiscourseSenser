9f159bf2fa86723cced06dccb9edf5584af9dc10
Wladimir Sidorenko <wlsidorenko@gmail.com>
2016-07-15 09:51:17 +0200

    tests of Brown clusters simplified

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; mode: python; -*-
+
+##################################################################
+# Imports
+import os
+
+
+##################################################################
+# Constants
+DIRNAME = os.path.dirname(__file__)
+DATA_DIR = os.path.join(DIRNAME, "data")
+TEST_BROWN_PATH = os.path.join(DATA_DIR, "brown_cluster_1000.txt")
